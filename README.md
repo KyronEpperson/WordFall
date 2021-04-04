8637e80d821b49555e91e442e88b2023bf685903
KyronEpperson <epperson.kyron@gmail.com>
2021-04-03 23:45:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WordFall
+ TypingGame
---
+++ WordFall/Assets/scripts/WordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    private static string[] wordList = {"join", "lemon", "ice", "fantastic", "rustic", "economic", "available", "verdict", "representative", "genius", "blessed", "jazzy", "desk", "informat",
+            "applejuice", "nightime", "collections", "headboard", "pepper", "music", "hungry", "nice", "basketball", "football", "track", "soccer", "volleyball", "synthesizer", "randomized", "destined", "honeymoon", "grandmother" };
+
+    public static string GetRandomWord ()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+            return randomWord;
+    }
+}
