e3bca991d8d6ac6c14d3b5018cd5eaca8e9f7b70
KyronEpperson <epperson.kyron@gmail.com>
2021-04-04 00:01:44 -0500

    in progress

--- WordFall/Assets/PauseMenu.cs
+++ WordFall/Assets/PauseMenu.cs
@@ -9,6 +9,12 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+   // [SerializeField]
+   // private Toggle toggle;
+    //[SerializeField]
+   // private AudioSource myAudio;
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -43,7 +49,128 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
     }
 
+    /*
     //public void load
+    public void LoadGame()
+    {
+        // 1
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            //ClearBullets();
+            //ClearRobots();
+            //RefreshRobots();
+
+            // 2
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+
+            /*
+            // 3
+            for (int i = 0; i < save.livingTargetPositions.Count; i++)
+            {
+                int position = save.livingTargetPositions[i];
+                Target target = targets[position].GetComponent<Target>();
+                target.ActivateRobot((RobotTypes)save.livingTargetsTypes[i]);
+                target.GetComponent<Target>().ResetDeathTimer();
+            }
+
+            // 4
+            shotsText.text = "Shots: " + save.shots;
+            hitsText.text = "Hits: " + save.hits;
+            shots = save.shots;
+            hits = save.hits;
+            
+
+
+            Debug.Log("Game Loaded");
+
+            Unpause();
+        }
+        else
+        {
+            Debug.Log("No game saved!");
+        }
+    }
+
     //public void save
-    //public void toggleMusic
+    public void SaveGame()
+    {
+        // 1
+        Save save = CreateSaveGameObject();
+
+        // 2
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        
+        // 3
+        hits = 0;
+        shots = 0;
+        shotsText.text = "Shots: " + shots;
+        hitsText.text = "Hits: " + hits;
+
+        ClearRobots();
+        ClearBullets();
+        Debug.Log("Game Saved");
+        
+    }
+
+    //save with Json
+    public void SaveAsJSON()
+    {
+        Save save = CreateSaveGameObject();
+        string json = JsonUtility.ToJson(save);
+
+        Debug.Log("Saving as JSON: " + json);
+    }
+
+
+
+   
+    public void ToggleMusic()
+    {
+        if (toggle.isOn)
+        {
+            PlayerPrefs.SetInt("music", 1);
+            myAudio.enabled = true;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("music", 0);
+            myAudio.enabled = false;
+        }
+        PlayerPrefs.Save();
+    }
+  
+
+    public void Awake()
+    {
+        if (!PlayerPrefs.HasKey("music"))
+        {
+            PlayerPrefs.SetInt("music", 1);
+            toggle.isOn = true;
+            myAudio.enabled = true;
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            if (PlayerPrefs.GetInt("music") == 0)
+            {
+                myAudio.enabled = false;
+                toggle.isOn = false;
+            }
+            else
+            {
+                myAudio.enabled = true;
+                toggle.isOn = true;
+            }
+        }
+    }
+  */
+
+
 }
